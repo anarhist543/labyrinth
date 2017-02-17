df045efbb72f5ace474b8f261fd46d59393086d6
Николай <rfka1@inbox.ru>
2017-02-17 11:39:15 +0300

    labyrinth v1

---
+++ Assets/Labirynth/Labyrinth.cs
@@ -0,0 +1,168 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Labyrinth {
+
+	Square[,] squareArray;
+
+	int width, height, sectorsNum, currentY = 0;
+	float screenWidth;
+	public GameObject squarePrefab;
+	Vector3 currentPosition;
+	bool activated = false;
+	bool first = true;
+
+	/// <summary>
+	/// Функция задающая параметры лабиринта
+	/// </summary>
+	/// <param name="w">кол-во квадратов по горизонтали</param>
+	/// <param name="h">кол-во квадратов по вертикали(в секторе)</param>
+	/// <param name="n">количество секторов</param>
+	/// <param name="screenW">размер экрана по горизонтали(в юнитах)</param>
+	/// <param name="squarePref">префаб квадрата</param>
+	/// <param name="startPos">позиция где должен находиться левый нижний угол лабиринта</param>
+	public void SetParams (int w, int h, int n, float screenW, GameObject squarePref, Vector3 startPos){
+		if (!activated) {
+			width = w;
+			height = h;
+			sectorsNum = n;
+			screenWidth = screenW;
+			squarePrefab = squarePref;
+			squareArray = new Square[h*n, w];
+			currentPosition = startPos + new Vector3 (0.5f, 0.5f, 0) * screenW / w;
+		} else {
+			Debug.LogError ("У лабиринта не заданны параметры(функция SetParams)");
+		}
+	}
+
+	/// <summary>
+	/// Генерирует следующий сектор и удаляет тот, сектор который был создан раньше всех
+	/// </summary>
+	public void GenerateNextSector ()
+	{
+		Square[,] sector = GenerateSector ();
+		for (int i = currentY; i < currentY + height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (squareArray [i, j] != null) {
+					Object.Destroy (squareArray [i, j].gameObject);
+				}
+				squareArray [i, j] = sector [i - currentY, j];
+				squareArray [i, j].posY = i;
+				squareArray [i, j].posX = j;
+			}
+		}
+		currentY = currentY + height;
+		if (currentY == height * sectorsNum)
+			currentY = 0;
+	}
+
+	Square[,] GenerateSector()
+	{
+		if (currentY == 0) {
+			if(squareArray [currentY, 0])
+				currentPosition = squareArray [height * sectorsNum - 1, 0].transform.position + Vector3.up * screenWidth / width;
+		} else if (squareArray [currentY, 0]) {
+			currentPosition = squareArray [currentY-1, 0].transform.position + Vector3.up * screenWidth / width;
+		}
+		Square[,] result = new Square[height, width];
+		for(int i = 0; i < height; i++){
+			for (int j = 0; j < width; j++) {
+				result [i, j] = Object.Instantiate (squarePrefab).GetComponent<Square> ();
+				result [i, j].Start ();
+				result [i, j].transform.localScale = Vector3.one * screenWidth / width;
+				result [i, j].transform.position = currentPosition;
+				currentPosition += Vector3.right * screenWidth / width;
+			}
+			currentPosition = result[i, 0].transform.position + Vector3.up * screenWidth / width;
+		}
+		int n = 0;
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (!first) {
+					if (i < height - 1) {
+						if (j < width - 1) {
+							if (Random.Range (0, 2) == 0) {
+								result [i, j].RemoveRight ();
+								result [i, j + 1].RemoveLeft ();
+								n++;
+							} else {
+								int k = Random.Range (j - n, j + 1);
+								result [i, k].RemoveDown ();
+								if (i > 0) {
+									result [i - 1, k].RemoveUp ();
+								}
+								n = 0;
+							}
+						} else {
+							int k1 = Random.Range (j - n, j + 1);
+							result [i, k1].RemoveDown ();
+							if (i > 0) {
+								result [i - 1, k1].RemoveUp ();
+							}
+							n = 0;
+						}
+					} else {
+						if (j < width - 1) {
+							result [i, j].RemoveRight ();
+							result [i, j + 1].RemoveLeft ();
+						}
+						result [i, j].RemoveUp ();
+					}
+				} else {
+					if (j < width - 1) {
+						result [i, j].RemoveRight ();
+						result [i, j + 1].RemoveLeft ();
+					}
+				}
+			}
+			first = false;
+		}
+		int k2 = Random.Range (0, width);
+		result [height - 2, k2].RemoveUp ();
+		result [height - 1, k2].RemoveDown ();
+		return result;
+	}
+
+	/// <summary>
+	/// Проверяет можно ли безприпятственно пройти из квадрата _from в квадрат _to
+	/// </summary>
+	/// <returns><c>true</c> если можно<c>false</c> если нельзя</returns>
+	public bool CanGo(Square _from, Square _to)
+	{
+		if (_from == _to)
+			return false;
+		if (_from.posX == _to.posX) {
+			bool b = true;
+			if (_from.posY > _to.posY) {
+				for (int i = _to.posY; i < _from.posY - 1; i++) {
+					b = b && !squareArray [i, _from.posX].up && !squareArray [i+1, _from.posX].down;
+				}
+			} else {
+				for (int i = _from.posY; i < _from.posY - 1; i++) {
+					b = b && !squareArray [i, _from.posX].up && !squareArray [i+1, _from.posX].down;
+				}
+			}
+			return b;
+		}
+		if (_from.posY == _to.posY) {
+			bool b = true;
+			if (_from.posX > _to.posX) {
+				for (int i = _to.posX; i < _from.posX - 1; i++) {
+					b = b && !squareArray [_from.posY, i].right  && !squareArray [_from.posY, i+1].left;
+				}
+			} else {
+				for (int i = _from.posX; i < _from.posX - 1; i++) {
+					b = b && !squareArray [_from.posY, i].right  && !squareArray [_from.posY, i+1].left;
+				}
+			}
+			return b;
+		}
+		return false;
+	}
+
+	public void Move (Vector3 dir){
+		foreach (Square s in squareArray) {
+			s.transform.position += dir;
+		}
+	}
+}
---
+++ Assets/Labirynth/Square.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Square : MonoBehaviour {
+
+
+	public bool up, right, down, left;
+
+	private Transform upLine, downLine, rightLine, leftLine;
+
+	/// <summary>
+	/// Позиция y квадрата в лабиринте.
+	/// </summary>
+	public int posY;
+
+	/// <summary>
+	/// Позиция x квадрата в лабиринте.
+	/// </summary>
+	public int posX;
+
+	public void Start(){
+		up = right = down = left = true;
+		upLine = transform.Find ("upLine");
+		downLine = transform.Find ("downLine");
+		rightLine = transform.Find ("rightLine");
+		leftLine = transform.Find ("leftLine");
+		upLine.transform.position = transform.position + Vector3.up * transform.localScale.x * 0.5f - Vector3.forward*0.1f;
+		downLine.transform.position = transform.position + Vector3.down * transform.localScale.x * 0.5f - Vector3.forward*0.1f;
+		rightLine.transform.position = transform.position + Vector3.right * transform.localScale.x * 0.5f - Vector3.forward*0.1f;
+		leftLine.transform.position = transform.position + Vector3.left * transform.localScale.x * 0.5f - Vector3.forward*0.1f;
+	}
+
+	/// <summary>
+	/// Устанавливает размер стороны квадрата в юнитах
+	/// </summary>
+	/// <param name="s">количество юнитов</param>
+	public void SetSize(float s){
+		transform.localScale = new Vector3 (s, s, 1);
+		if (upLine) {
+			upLine.transform.position = transform.position + Vector3.up * s * 0.5f;
+		}
+		if (downLine) {
+			downLine.transform.position = transform.position + Vector3.down * s * 0.5f;
+		}
+		if (rightLine) {
+			rightLine.transform.position = transform.position + Vector3.right * s * 0.5f;
+		}
+		if (leftLine) {
+			leftLine.transform.position = transform.position + Vector3.left * s * 0.5f;
+		}
+	}
+
+
+	/// <summary>
+	/// Удаляет правую сторону квадрата
+	/// </summary>
+	public void RemoveRight()
+	{
+		if (right) {
+			right = false;
+			Destroy (rightLine.gameObject);
+		}
+	}
+
+	/// <summary>
+	/// Удаляет левую сторону квадрата
+	/// </summary>
+	public void RemoveLeft()
+	{
+		if (left) {
+			left = false;
+			Destroy (leftLine.gameObject);
+		}
+	}
+
+	/// <summary>
+	/// Удаляет верхнюю сторону квадрата
+	/// </summary>
+	public void RemoveUp()
+	{
+		if (up) {
+			up = false;
+			Destroy (upLine.gameObject);
+		}
+	}
+
+	/// <summary>
+	/// Удаляет нижнюю сторону квадрата
+	/// </summary>
+	public void RemoveDown()
+	{
+		if (down) {
+			down = false;
+			Destroy (downLine.gameObject);
+		}
+	}
+
+}
