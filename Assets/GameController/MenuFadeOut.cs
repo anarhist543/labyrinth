6fa047f6d1323583610389612d3f7d50d319b8cf
Dartstriker <linedward.work@gmail.com>
2017-02-23 16:12:37 +0300

    gamecontroller v1 r

---
+++ Assets/GameController/MenuFadeOut.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuFadeOut : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		foreach (Transform child in transform)
+		{
+			if (child.GetComponent<CanvasRenderer>() != null) child.GetComponent<CanvasRenderer>().SetAlpha(GameController.Instance.GetUIOpacity());
+			foreach (Transform child2 in child)
+			{
+				if (child2.GetComponent<CanvasRenderer>() != null) child2.GetComponent<CanvasRenderer>().SetAlpha(GameController.Instance.GetUIOpacity());
+			}
+		}
+
+
+		
+	}
+}
