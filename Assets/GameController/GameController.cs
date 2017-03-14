416c5e9159ec024fa984934b4909dbf089f7b784
Николай <rfka1@inbox.ru>
2017-03-14 11:18:08 +0300

    t

---
+++ Assets/GameController/GameController.cs
@@ -0,0 +1,219 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public Camera gameCamera;
+	public GameObject squarePrefab;
+
+	public static GameController Instance;
+	public Labyrinth labyrinthInstance = new Labyrinth ();
+
+	public float startingSpeed;
+	public float acceleration;
+	private float currentSpeed;
+	private float generateDistance;
+	private float currentDistance = 0;
+	private float score;
+	private float timePassedGame;
+	private bool isPlaying;
+	private bool isCourutineActive = false;
+	private GameObject ball;
+	public GameObject ballPrefab;
+	public int gridWidth;
+	public int gridHeight;
+	public int startingGenSectors;
+
+	public int adsPixelSize;
+	private float screenHeightUnits, screenHeightPixels;
+	private float screenWidthUnits, screenWidthPixels;
+	public int passedSectorsOnStart;
+	public float speedOnstart;
+
+	//This will be used for displaying main menu
+	private float opacityUI = 1f;
+	public float fadeOutTime;
+
+	private float timer;
+
+<<<<<<< HEAD:Assets/GameController/Scripts/GameController.cs
+	public float SCORE_CONSTANT = 1f;
+
+	public void GetBall (GameObject _ball)
+	{
+		ball = _ball;
+	}
+
+	void OnDrawGizmos ()
+	{
+		Vector3 bottomLeftCorner = gameCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 10f));
+		Vector3 upperRightCorner = gameCamera.ScreenToWorldPoint(new Vector3(gameCamera.pixelWidth, gameCamera.pixelHeight, 10f));
+=======
+	private int PixelHeight;
+>>>>>>> 09c184f936a69da24877debdcdc9fd225a1d1a7f:Assets/GameController/GameController.cs
+
+	static float SCORE_CONSTANT = 1f;
+
+	void Init ()
+	{
+		Vector3 bottomLeftCorner = gameCamera.ScreenToWorldPoint(new Vector3(0f, 0f, 10f));
+		Vector3 upperRightCorner = gameCamera.ScreenToWorldPoint(new Vector3(gameCamera.pixelWidth, gameCamera.pixelHeight, 10f));
+
+<<<<<<< HEAD:Assets/GameController/Scripts/GameController.cs
+		screenWidthUnits = -bottomLeftCorner.x + upperRightCorner.x;
+		screenHeightUnits = upperRightCorner.y - bottomLeftCorner.y;
+		screenWidthPixels = gameCamera.pixelWidth;
+		screenHeightPixels = gameCamera.pixelHeight;
+=======
+		PixelHeight = gameCamera.pixelHeight;
+
+		float screenWidthUnits = -bottomLeftCorner.x + upperRightCorner.x;
+>>>>>>> 09c184f936a69da24877debdcdc9fd225a1d1a7f:Assets/GameController/GameController.cs
+		generateDistance = screenWidthUnits / gridWidth * gridHeight;
+		labyrinthInstance.SetParams (gridWidth, gridHeight, startingGenSectors, screenWidthUnits, squarePrefab, bottomLeftCorner); 
+		for (int i = 0; i < startingGenSectors; i++) {
+			labyrinthInstance.GenerateNextSector ();
+		}
+	}
+
+	public int GetScore ()
+	{
+		return Mathf.FloorToInt(score);
+	}
+
+	public float GetUIOpacity ()
+	{
+		return opacityUI;
+	}
+
+	public int GetPixelHeight ()
+	{
+		return PixelHeight;
+	}
+
+	void Awake ()
+	{
+		if (Instance != null)
+		{
+			Destroy (gameObject);
+		}
+		else 
+		{
+			Instance = this;
+			DontDestroyOnLoad (gameObject);
+		}
+	}
+
+	void Start ()
+	{
+		Init();
+		isPlaying = false;
+	}
+
+	void Update ()
+	{
+		if (isCourutineActive) 
+		{
+			//do nothing (?)
+		}
+		else if (isPlaying) MainPlayLoop ();
+		else MainMenuScrollingBackground ();
+	}
+
+	void MainMenuScrollingBackground ()
+	{
+		currentSpeed = startingSpeed;
+		labyrinthInstance.Move (Vector3.down * currentSpeed * Time.deltaTime);
+		currentDistance += currentSpeed * Time.deltaTime;
+
+		if (currentDistance >= generateDistance)
+		{
+			currentDistance -= generateDistance;
+			labyrinthInstance.GenerateNextSector ();
+		}
+	}
+
+	public void GameStart ()
+	{
+		if (isPlaying || isCourutineActive)
+			return;
+		isCourutineActive = true;
+		labyrinthInstance.GenerateBall (passedSectorsOnStart - 3, ballPrefab);
+		StartCoroutine(StartingMove(0f, passedSectorsOnStart*generateDistance - currentDistance - generateDistance /gridHeight - HeightPixelsToUnits(50)));
+		score = 0;
+		timer = 0;
+
+	}
+
+	public IEnumerator StartingMove (float a, float b) //currentDistance - generateDistance?
+	{
+		while (a < b)
+		{
+			Debug.Log ("active");
+
+			float completion = 1f - a / b;
+
+			if (ball != null)
+				ball.transform.position += Vector3.down * (speedOnstart * completion * Time.deltaTime + 0.5f);
+
+			labyrinthInstance.Move (Vector3.down * (speedOnstart * completion * Time.deltaTime + 0.5f));
+			currentDistance += speedOnstart * completion * Time.deltaTime + 0.5f;
+
+			if (currentDistance >= generateDistance)
+			{
+				currentDistance -= generateDistance;
+				labyrinthInstance.GenerateNextSector ();
+			}
+
+			a += speedOnstart * completion * Time.deltaTime + 0.5f;
+
+			opacityUI = completion;
+
+			yield return null;
+		}
+		StartCoroutine (Countdown (3));
+	}
+
+	IEnumerator Countdown(int c)
+	{
+		while (c > 0) {
+			score = c;
+			c--;
+			yield return new WaitForSeconds (1f);
+		}
+		Debug.Log ("Started");
+		isCourutineActive = false;
+		isPlaying = true;
+	}
+
+	public bool CanGo(Square _from, Square _to)
+	{
+		return labyrinthInstance.CanGo (_from, _to);
+	}
+
+	void MainPlayLoop ()
+	{
+		timePassedGame += Time.deltaTime;
+
+		currentSpeed += acceleration * Time.deltaTime;
+
+		if (ball != null)
+			ball.transform.position += Vector3.down * currentSpeed * Time.deltaTime;
+		labyrinthInstance.Move (Vector3.down * currentSpeed * Time.deltaTime);
+		currentDistance += currentSpeed * Time.deltaTime;
+		score += currentSpeed * Time.deltaTime * SCORE_CONSTANT;
+
+		Debug.Log(score);
+
+		if (currentDistance >= generateDistance)
+		{
+			currentDistance -= generateDistance;
+			labyrinthInstance.GenerateNextSector ();
+		}
+	}
+
+	float HeightPixelsToUnits(int pixels)
+	{
+		return pixels * screenHeightUnits / (float)screenHeightPixels;
+	}
+}
---
+++ Assets/GameController/GameOverScreen.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScreen : MonoBehaviour
+{
+	public Text scoreText;
+	public Text highScoreText;
+	public Text newRecordText;
+	private int offset;
+	RectTransform rect;
+
+	static int UI_HEIGHT = 1920;
+
+	void Awake ()
+	{
+		rect = GetComponent <RectTransform> ();
+	}
+
+	void OnEnable ()
+	{
+		offset = UI_HEIGHT;
+
+		scoreText.text = "YOUR SCORE IS\n" + GameController.Instance.GetScore() + " POINTS";
+
+		highScoreText.text = "HIGHSCORE IS\n" + PrefsGetSavedRecord() + " POINTS";
+
+		if (PrefsGetSavedRecord() < GameController.Instance.GetScore())
+		{
+			newRecordText.text = "NEW RECORD!";
+			PrefsSaveRecord();
+		}
+		else newRecordText.text = " ";
+
+		StartCoroutine(Coroutine1());
+	}
+
+	/// <summary>
+	/// Функция, записывающая очки на момент вызова в память телефона
+	/// </summary>
+	void PrefsSaveRecord ()
+	{
+		if (GameController.Instance.GetScore() > PrefsGetSavedRecord()) // Добавлено для безопасности. Не нужно перезаписывать рекорд, если он меньше.
+		{
+		PlayerPrefs.SetInt("Highscore", GameController.Instance.GetScore());
+		}
+	}
+
+	/// <summary>
+	/// Функция, возвращающая записанное в памяти значение рекорда
+	/// </summary>
+	int PrefsGetSavedRecord ()
+	{
+		return PlayerPrefs.GetInt("Highscore");
+	}
+
+	public IEnumerator Coroutine1 ()
+	{
+		int speed = 0;
+
+		while (offset > 0)
+		{
+			speed += 5;
+			offset -= Mathf.Min(speed, offset);
+			yield return new WaitForEndOfFrame();
+		}
+		if (speed-35 > 0) StartCoroutine(Coroutine2(speed-35));
+		yield return null;
+	}
+
+	public IEnumerator Coroutine2 (int speed)
+	{
+		do
+		{
+			speed -= 5;
+			offset += speed;
+			yield return new WaitForEndOfFrame();
+		}
+		while (offset > 0);
+		if (-speed - 15 > 0) StartCoroutine(Coroutine2(-speed - 15));
+		yield return null;
+	}
+
+	void Update ()
+	{
+		rect.offsetMin = new Vector2 (rect.offsetMin.x, offset);
+		rect.offsetMax = new Vector2 (rect.offsetMin.x, offset);
+		// Данный кусок кода отвечает за сдвиг UI, каждый кадр устанавливая его на значение "offset"
+	}
+}
